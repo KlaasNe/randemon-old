413668f3e6d81edb7a5f0c28a8bb068d84299ebe
Bryant K. Nelson <bryant.nelson@ibm.com>
2020-07-15 11:32:03 -0500

    Add input parsing.

---
+++ generator/inputs.py
@@ -0,0 +1,42 @@
+import argparse
+
+def make_parser():
+    """Make the ArgParser object."""
+    parser = argparse.ArgumentParser(
+        description=
+        'Landscape generator using the tileset of pokemon fire red')
+
+    parser.add_argument(
+        '--width',
+        dest='map_size_x',
+        type=int,
+        default=120,
+        help='The horizontal amount of tiles the map consists of. full hd -> 120x68')
+
+    parser.add_argument(
+        '--height',
+        dest='map_size_y',
+        type=int,
+        default=68,
+        help='The vertical amount of tiles the map consists of. full hd -> 120x68')
+
+    parser.add_argument(
+        '--headless',
+        dest='headless_opt',
+        action='store_true',
+        help='Run in headless mode.')
+
+    parser.add_argument(
+        '--save',
+        dest='save_opt',
+        action='store_true',
+        help='Save generated image.')
+
+    parser.add_argument(
+        '--seed',
+        dest='seed_opt',
+        type=int,
+        default=69420,
+        help='The horizontal amount of tiles the map consists of. full hd -> 120x68')
+
+    return parser
