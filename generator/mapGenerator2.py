d45d0de0a6a3085b3b652949b2af64b7412ef031
KlaasNeyens <klaas.neyens@gmail.com>
2020-08-02 18:19:05 +0200

    woops

--- generator/mapGenerator2.py
+++ generator/mapGenerator2.py
@@ -202,7 +202,7 @@ if not args.credits_opt:
     apply_path_sprites(random_map)
 
     create_hill_edges(random_map, update=True)
-    create_trees(random_map, random.tree_coverage, x_offset, y_offset)
+    create_trees(random_map, random_map.tree_coverage, x_offset, y_offset)
     all_pokemon = spawn_pokemons(random_map)
     spawn_npc(random_map, 1)
     create_lanterns(random_map)
